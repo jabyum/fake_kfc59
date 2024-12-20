8c97a0dda102ca0fcde4167db786781270c98fb3
jabyum <jabyum@github.com>
2024-12-20 18:44:39 +0500

    second

--- database.py
+++ database.py
@@ -16,7 +16,9 @@ sql.execute("CREATE TABLE IF NOT EXISTS cart "
             "pr_name TEXT, pr_count INTEGER, "
             "total_price REAL);")
 connection.commit()
-
+#TODO дз получение айди всех юзеров бота
+def get_all_id():
+    pass
 def add_user(name, phone_number, user_id):
     connection = sqlite3.connect("data.db")
     sql = connection.cursor()
@@ -59,7 +61,7 @@ def delete_exact_product(pr_id):
     sql = connection.cursor()
     sql.execute("DELETE FROM products WHERE pr_id = ?;", (pr_id, ))
     connection.commit()
-# дз функция изменения количества определенного продукта
+#TODO дз функция изменения количества определенного продукта
 def change_quantity(something):
         pass
 
@@ -69,6 +71,36 @@ def delete_all_products():
     sql.execute("DELETE FROM products;")
     connection.commit()
 
+def get_pr_id_name():
+    connection = sqlite3.connect("data.db")
+    sql = connection.cursor()
+    # берем для кнопок продуктов нужную информацию
+    all_product = sql.execute("SELECT pr_id, pr_name, pr_quantity "
+                              "FROM products;").fetchall()
+    # фильтрация продуктов, оставляем те, что в количестве больше 0
+    #[(pr1_id, pr1_name, pr1_quantity), (pr2_id, pr2_name, pr2_quantity), ...]
+    actual_products = [[product[0], product[1]] for product
+                       in all_product if product[2] > 0]
+    return actual_products
+# работа с корзиной
+def add_to_cart(user_id, pr_id, pr_name, pr_price, pr_quantity):
+    connection = sqlite3.connect("data.db")
+    sql = connection.cursor()
+    total_price = pr_price * pr_quantity
+    sql.execute("INSERT INTO cart (user_id, pr_id, pr_name, pr_count, "
+                "total_price) VALUES (?, ? ,? ,?, ?);", (user_id, pr_id,
+                                                         pr_name, pr_quantity,
+                                                         total_price))
+    connection.commit()
+def delete_user_cart(user_id):
+    connection = sqlite3.connect("data.db")
+    sql = connection.cursor()
+    sql.execute("DELETE FROM cart WHERE user_id=?;", (user_id, ))
+    connection.commit()
+#TODO дз получение информации о корзине юзера
+# формат информации (название продуктов, количество, общая цена за это количество)
+def get_user_cart(something):
+    pass
 
 
 
